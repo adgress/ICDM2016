04806860a27709d402fbfc355f2cf309669b0d5e
adgress <agress@ucdavis.edu>
2016-12-06 18:54:47 -0800

    Implemented similar and relative guidance

---
+++ main.py
@@ -0,0 +1,139 @@
+
+import scipy_optimize
+from scipy import optimize
+import numpy as np
+from numpy.linalg import norm
+from sklearn.preprocessing import StandardScaler
+
+def run_optimization(eval, grad, w0):
+    options = dict()
+    options['maxiter'] = 1000
+    options['disp'] = False
+    method = 'L-BFGS-B'
+    err = optimize.check_grad(eval, grad, w0)
+
+    g = grad(w0)
+    g_approx = optimize.approx_fprime(w0, eval, 1e-8)
+    print err
+    print norm(g - g_approx) / norm(g)
+    results = optimize.minimize(eval, w0, method=method, jac=grad, options=options)
+    w, b = scipy_optimize.unpack_linear(results.x)
+    return w, b
+
+def constraints_bound(x, y, x_bound, lower_bound, upper_bound, C, C2):
+    transform = StandardScaler()
+    x = transform.fit_transform(x)
+    opt_data = scipy_optimize.optimize_data(
+        x, y, C, C2
+    )
+    opt_data.x_bound = x_bound
+    opt_data.bounds = np.stack((lower_bound, upper_bound), 1)
+    eval = scipy_optimize.logistic_bound.create_eval(opt_data)
+    grad = scipy_optimize.logistic_bound.create_grad(opt_data)
+    w0 = np.zeros(x.shape[1] + 1)
+    return run_optimization(eval, grad, w0)
+
+def constraints_pairwise(x, y, x_low, x_high, C, C2, relative=True, max_delta=None):
+    transform = StandardScaler()
+    x = transform.fit_transform(x)
+    x_low = transform.transform(x_low)
+    x_high = transform.transform(x_high)
+    opt_data = scipy_optimize.optimize_data(
+        x, y, C, C2
+    )
+    opt_data.x_low = x_low
+    opt_data.x_high = x_high
+    w0 = np.zeros(x.shape[1] + 1)
+    if relative:
+        eval = scipy_optimize.logistic_pairwise.create_eval(opt_data)
+        grad = scipy_optimize.logistic_pairwise.create_grad(opt_data)
+    else:
+        opt_data.s = max_delta
+        eval = scipy_optimize.logistic_similar.create_eval(opt_data)
+        grad = scipy_optimize.logistic_similar.create_grad(opt_data)
+    return run_optimization(eval, grad, w0)
+
+def random_xy(w):
+    p = w.size
+    x = np.random.normal(0, 1, (1, p))
+    y = x.dot(w) + np.random.normal(0, 1)
+    return x, y
+
+def create_random_bound(w, n, delta):
+    p = w.size
+    max_iterations = 10000
+    x = np.zeros((n, p))
+    y = np.zeros(n)
+    upper = np.zeros(n)
+    lower = np.zeros(n)
+    for i in range(n):
+        xi, yi = random_xy(w)
+        x[i, :] = xi
+        lower[i] = yi - delta
+        upper[i] = yi - delta
+    return x, lower, upper
+
+def create_random_similar(w, n, max_delta):
+    p = w.size
+    max_iterations = 10000
+    x_similar1 = np.zeros((n, p))
+    x_similar2 = np.zeros((n, p))
+    iter = 0
+    idx = 0
+    while iter < max_iterations and idx < n:
+        x1, y1 = random_xy(w)
+        x2, y2 = random_xy(w)
+        iter += 1
+        if np.abs(y1-y2) > max_delta:
+            continue
+        if y1 > y2:
+            x_similar1[idx, :] = x2
+            x_similar2[idx, :] = x1
+        else:
+            x_similar1[idx, :] = x1
+            x_similar2[idx, :] = x2
+        x_similar1[idx, :] = x1
+        x_similar2[idx, :] = x2
+        idx += 1
+    if idx != n:
+        print 'Failed to produce enough similar constraints'
+    return x_similar1, x_similar2
+
+
+def create_random_relative(w, n):
+    p = w.size
+    x_low = np.zeros((n, p))
+    x_high = np.zeros((n, p))
+    for i in range(n):
+        x1, y1 = random_xy(w)
+        x2, y2 = random_xy(w)
+        if y1 > y2:
+            x_high[i, :] = x1
+            x_low[i, :] = x2
+        else:
+            x_high[i, :] = x2
+            x_low[i, :] = x1
+    return x_high, x_low
+
+
+if __name__ == '__main__':
+    p = 50
+    n = 20
+    n_mixed = 40
+    C = 1
+    C2 = 1
+    w = np.random.normal(0, 1, p)
+    x = np.random.normal(0, 1, (n, p))
+    y = x.dot(w) + np.random.normal(0, 1, n)
+
+    x_high, x_low = create_random_relative(w, n_mixed)
+    w_relative = constraints_pairwise(x, y, x_low, x_high, C, C2)
+
+    max_delta = .2*np.abs(y.max() - y.min())
+    x_similar1, x_similar2 = create_random_similar(w, n_mixed, max_delta)
+    w_relative = constraints_pairwise(x, y, x_similar1, x_similar2, C, C2, relative=True, max_delta=max_delta)
+
+    x_bound, lower_bound, upper_bound = create_random_bound(w, n_mixed, y.std())
+    w_bound = constraints_bound(x, y, x_bound, lower_bound, upper_bound, C, C2)
+
+    print ''
---
+++ scipy_optimize.py
@@ -0,0 +1,408 @@
+import numpy as np
+from numpy.linalg import norm
+import scipy.optimize as optimize
+from scipy.special import expit as sigmoid
+
+def relative_error(v1, v2):
+    return norm(v1-v2)/norm(v1)
+
+
+#from http://stackoverflow.com/questions/4474395/staticmethod-and-abc-abstractmethod-will-it-blend
+class abstractstatic(staticmethod):
+    __slots__ = ()
+    def __init__(self, function):
+        super(abstractstatic, self).__init__(function)
+        function.__isabstractmethod__ = True
+    __isabstractmethod__ = True
+
+def pack_linear(w,b):
+    return np.append(w,b)
+
+def unpack_linear(v):
+    return v[0:v.size-1], v[v.size-1]
+
+def eval_reg_l2(w):
+    return norm(w)**2
+
+def grad_reg_l2(w):
+    return 2*w
+
+def loss_l2(y1, y2):
+    v = norm(y1-y2)**2
+    v_n = v / y1.size
+    return v_n
+    #return v
+
+def grad_linear_loss_l2(x, y, v):
+    w,b = unpack_linear(v)
+    n = y.size
+    xw = x.dot(w)
+    grad_w = 2*(x.T.dot(xw) - x.T.dot(y) + 2*x.T.sum(1)*b)
+    grad_b = 2*(xw.sum() - y.sum() + n*b)
+    grad_w_n = grad_w/n
+    grad_b_n = grad_b/n
+    return pack_linear(grad_w_n, grad_b_n)
+    #return pack_linear(grad_w, grad_b)
+
+def apply_linear(x, w, b=None):
+    if b is None:
+        w,b = unpack_linear(w)
+    return x.dot(w) + b
+
+def eval_linear_loss_l2(x, y, v):
+    w, b = unpack_linear(v)
+    return loss_l2(apply_linear(x, w, b), y)
+
+
+class optimize_data(object):
+    def __init__(self, x, y, reg, reg_mixed):
+        self.x = x
+        self.y = y
+        self.reg = reg
+        self.reg_mixed = reg_mixed
+
+    def get_xy(self):
+        return self.x, self.y
+
+    def get_reg(self):
+        return self.reg, self.reg_mixed
+
+
+class logistic_optimize(object):
+    @abstractstatic
+    def eval_mixed_guidance(data, v):
+        pass
+
+    @abstractstatic
+    def grad_mixed_guidance(data, v):
+        pass
+
+    @abstractstatic
+    def _grad_num_mixed_guidance(data, v):
+        pass
+
+    @classmethod
+    def eval(cls, data, v):
+        eval_loss = cls.eval_loss(data, v)
+        eval_reg = cls.eval_reg(data, v)
+        reg, reg_mixed = data.get_reg()
+        val = eval_loss + reg*eval_reg
+        if reg_mixed > 0:
+            eval_mixed = cls.eval_mixed_guidance(data, v)
+            val += eval_mixed*reg_mixed
+        return val
+
+    @staticmethod
+    def eval_loss(data, v):
+        x, y = data.get_xy()
+        return eval_linear_loss_l2(x, y, v)
+
+    @staticmethod
+    def grad_loss(data, v):
+        x, y = data.get_xy()
+        return grad_linear_loss_l2(x, y, v)
+
+    @staticmethod
+    def eval_reg(data, v):
+        w, b = unpack_linear(v)
+        return eval_reg_l2(w)
+
+    @staticmethod
+    def grad_reg(data, v):
+        w, b = unpack_linear(v)
+        g = grad_reg_l2(w)
+        return np.append(g, 0)
+
+    @classmethod
+    def grad(cls, data, v):
+        grad_loss = cls.grad_loss(data, v)
+        reg, reg_mixed = data.get_reg()
+        grad_reg = cls.grad_reg(data, v)
+        grad_reg *= reg
+
+        I = np.isinf(reg_mixed) | np.isnan(reg_mixed)
+        if I.any():
+            print 'inf or nan!'
+            reg_mixed[I] = 0
+
+        val = grad_loss + grad_reg
+        if reg_mixed != 0:
+            grad_mixed = cls.grad_mixed_guidance(data, v)
+            val += reg_mixed * grad_mixed
+
+        return val
+
+    @classmethod
+    def create_eval(cls, data):
+        return lambda v: cls.eval(data, v)
+
+    @classmethod
+    def create_grad(cls, data):
+        return lambda v: cls.grad(data, v)
+
+class logistic_similar(logistic_optimize):
+    @staticmethod
+    def eval_mixed_guidance(data, v):
+        x1 = data.x1
+        x2 = data.x2
+        s = data.s
+        y1 = apply_linear(x1, v)
+        y2 = apply_linear(x2, v)
+        d = (y2 - y1)
+        denom = np.log(1 + np.exp(s+d) + np.exp(d-s) + np.exp(2*d))
+
+        vals = d - denom + np.log(np.exp(s) - np.exp(-s))
+        return -vals.sum()
+
+
+    @staticmethod
+    def grad_mixed_guidance(data, v):
+        x1 = data.x1
+        x2 = data.x2
+        s = data.s
+        y1 = apply_linear(x1, v)
+        y2 = apply_linear(x2, v)
+        d = (y2 - y1)
+        a = np.exp(s+d) + np.exp(d-s) + np.exp(2*d)
+        a2 = np.exp(s+d) + np.exp(d-s) + 2*np.exp(2*d)
+
+        dx = (x2 - x1)
+        I = np.isinf(a)
+        a[I] = 1
+        t = 1 - (a2/(1+a))
+        g_fast = (dx.T*t).sum(1)
+        g_fast = np.append(g_fast, 0)
+        #err = array_functions.relative_error(g, g_fast)
+        g = g_fast
+        return -g
+
+class logistic_pairwise(logistic_optimize):
+    @staticmethod
+    def eval_mixed_guidance(data, v):
+        x_low = data.x_low
+        x_high = data.x_high
+        if x_low is None:
+            return 0
+        yj = apply_linear(x_low, v)
+        yi = apply_linear(x_high, v)
+        d = (yi - yj)
+        '''
+        v = sigmoid(yi-yj)
+        v_log = -np.log(v)
+        return v_log.sum()
+        '''
+        vals = np.log(1 + np.exp(-d))
+        I = np.isinf(vals) | np.isnan(vals)
+        if I.any():
+            #print 'logistic_pairwise eval: inf! ' + str(I.mean())
+            #vals[np.isinf(vals)] = 1e16
+            pass
+        vals_mean = vals.mean()
+        return vals_mean
+        #vals_sum = vals.sum()
+        #return vals.sum()
+
+
+    @staticmethod
+    def grad_mixed_guidance(data, v):
+        x_low = data.x_low
+        x_high = data.x_high
+        if x_low is None:
+            return 0
+        n = x_low.shape[0]
+        d = (apply_linear(x_high, v) - apply_linear(x_low, v))
+        sig = sigmoid(d)
+        g = np.zeros(v.size)
+
+        dx = x_high - x_low
+        g_fast = (dx.T*(1-sig)).sum(1)
+        g_fast = np.append(g_fast, (1-sig).sum())
+        g = g_fast
+        #rel_err =  array_functions.relative_error(g,g_fast)
+        g *= -1
+        g[-1] *= 0
+        g_m = g / n
+        return g_m
+
+
+eps = 1e-2
+
+class logistic_neighbor(logistic_optimize):
+
+    @staticmethod
+    def eval_mixed_guidance(data, v):
+        assert False, "TODO: Normalize by amount of guidance"
+        x = data.x_neighbor
+        x_low = data.x_low
+        x_high = data.x_high
+        w, b = unpack_linear(v)
+        y = apply_linear(x, w, b)
+        y_low = apply_linear(x_low, w, b)
+        y_high = apply_linear(x_high, w, b)
+        '''
+        if (y_low + eps >= y_high).any() or (y + eps >= y_high).any():
+            return np.inf
+        '''
+        sig1 = sigmoid((y_high-y_low))
+        sig2 = sigmoid((2*y - y_high - y_low))
+        diff = sig1 - sig2
+        #assert (np.sign(diff) > 0).all()
+        small_constant = getattr(data,'eps',eps)
+        #assert False, 'Should this be infinity instead?'
+        #diff[diff < 0] = 0
+        vals2 = -np.log(diff + small_constant)
+        I = np.isnan(vals2)
+        if I.any():
+            #print 'eval_linear_neighbor_logistic: inf = ' + str(I.mean())
+            return np.inf
+        val2 = vals2.sum()
+        #assert norm(val - val2)/norm(val) < 1e-6
+        return val2
+
+    @staticmethod
+    def grad_mixed_guidance(data, v):
+        assert False, "TODO: Normalize by amount of guidance"
+        x = data.x_neighbor
+        x_low = data.x_low
+        x_high = data.x_high
+        w, b = unpack_linear(v)
+        y = apply_linear(x, w, b)
+        y_low = apply_linear(x_low, w, b)
+        y_high = apply_linear(x_high, w, b)
+
+        sig1 = sigmoid((y_high-y_low))
+        sig2 = sigmoid((2*y - y_high - y_low))
+        small_constant = getattr(data,'eps',eps)
+        diff = sig1 - sig2
+        #diff[diff < 0] = 0
+        denom = diff + small_constant
+        x1 = (x_high - x_low)
+        x2 = (2*x - x_low - x_high)
+
+        num1 = sig1*(1-sig1)
+        num2 = sig2*(1-sig2)
+        d = x1.T*num1 - x2.T*num2
+        g_fast = (d/denom).sum(1)
+        g_fast = np.append(g_fast, 0)
+
+        #err = array_functions.relative_error(val, g_fast)
+        val = g_fast
+        val *= -1
+        I = np.isnan(val) | np.isinf(val)
+        if I.any():
+            #print 'grad_linear_neighbor_logistic: nan!'
+            val[I] = 0
+        return val
+
+    @staticmethod
+    def _grad_num_mixed_guidance(x, x_low, x_high, w, b=None):
+        assert False
+        if b is None:
+            w,b = unpack_linear(w)
+
+
+        y = apply_linear(x, w, b)
+        y_low = apply_linear(x_low, w, b)
+        y_high = apply_linear(x_high, w, b)
+
+        a1 = y_high - y_low
+        a2 = 2*y-y_low-y_high
+
+        t1 = np.exp(-a1)*((1 + np.exp(-a1))**-2)
+        t2 = np.exp(-a2)*((1 + np.exp(-a2))**-2)
+
+        x1 = x_low - x_high
+        x2 = x_high + x_low - 2*x
+
+        return t1,x2,t2,x2
+
+    @staticmethod
+    def constraint_neighbor(v, x_low, x_high):
+        w,b = unpack_linear(v)
+        y_low = apply_linear(x_low,w,b)
+        y_high = apply_linear(x_high,w,b)
+        return y_high - y_low - eps
+
+    @staticmethod
+    def constraint_neighbor2(v, x, x_low, x_high):
+        w,b = unpack_linear(v)
+        y = apply_linear(x, w, b)
+        y_low = apply_linear(x_low,w,b)
+        y_high = apply_linear(x_high,w,b)
+        return y_high - y - eps
+
+    @staticmethod
+    def create_constraint_neighbor(x_low, x_high):
+        return lambda v: logistic_neighbor.constraint_neighbor(v, x_low, x_high)
+
+    @staticmethod
+    def create_constraint_neighbor2(x, x_low, x_high):
+        return lambda v: logistic_neighbor.constraint_neighbor2(v, x, x_low, x_high)
+
+
+class logistic_bound(logistic_optimize):
+    @staticmethod
+    def eval_mixed_guidance(data, v):
+        assert False, "TODO: Normalize by amount of guidance"
+        w, b = unpack_linear(v)
+        x = data.x_bound
+        bounds = data.bounds
+        y = apply_linear(x, w, b)
+        assert y.size == bounds.shape[0]
+        c1 = bounds[:, 0]
+        c2 = bounds[:, 1]
+
+        sig1 = sigmoid((c2-y))
+        sig2 = sigmoid((c1-y))
+        small_constant = getattr(data,'eps',eps)
+        diff = sig1 - sig2 + small_constant
+        vals2 = -np.log(sig1-sig2 + small_constant)
+        val2 = vals2.sum()
+        I = np.isnan(vals2)
+        if I.any():
+            assert False
+        #assert norm(val - val2)/norm(val) < 1e-6
+        return val2
+
+    @staticmethod
+    def _grad_num_mixed_guidance(x, c, w, b=None):
+        assert False
+        if b is None:
+            w,b = unpack_linear(w)
+        a = c - apply_linear(x, w, b)
+        t = 1 + np.exp(-a)
+        t = t ** -2
+        t2 = np.exp(-a)
+        return t*t2, x
+
+    @staticmethod
+    def grad_mixed_guidance(data, v):
+        bounds = data.bounds
+        x = data.x_bound
+        w, b = unpack_linear(v)
+        y = apply_linear(x, w, b)
+        assert y.size == bounds.shape[0]
+        c1 = bounds[:, 0]
+        c2 = bounds[:, 1]
+
+        sig1 = sigmoid((c2-y))
+        sig2 = sigmoid((c1-y))
+        small_constant = getattr(data,'eps',eps)
+        denom = sig1 - sig2 + small_constant
+
+
+        num = sig1*(1-sig1) - sig2*(1-sig2)
+        num /= denom
+        g_fast = (x.T*num).sum(1)
+        g_fast = np.append(g_fast, num.sum())
+        #rel_error = array_functions.relative_error(val, g_fast)
+        val = g_fast
+        if np.isnan(val).any():
+            print 'grad_linear_bound_logistic: nan!'
+            val[np.isnan(val)] = 0
+        if np.isinf(val).any():
+            #print 'grad_linear_bound_logistic: inf!'
+            val[np.isinf(val)] = 0
+        return val
+
+
